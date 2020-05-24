bea9dc7896a75d636678e472a5ec9a508cc502cc
Gautam Kumar <gautam.kriiest@gmail.com>
2020-05-24 15:18:00 +0530

    main and app .js

---
+++ src/components/Main.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from './Header';
+import Footer from './footer';
+function Main() {
+  return(
+    <div className="Main">
+      <Header />
+      <Footer />
+    </div>
+  );
+}
+export default Main;
